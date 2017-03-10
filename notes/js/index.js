a8f4af18eac254991aa61150b64a87cf4ccc3492
Mr-Plants <1171098609@qq.com>
2017-03-10 21:33:08 +0800

    3.10

--- notes/js/index.js
+++ notes/js/index.js
@@ -1,30 +1,40 @@
 var canSearch=true, //控制是否可以搜索  默认可以搜索
 	canMove=true,   //控制是否可以滑动删除  默认可以滑动
+	create=false,   //是否通过创建打开  默认不是
+	enter=true,	    //是否可以点击进入  默认可以		
 	isEdit=false;   //是否在编辑状态  默认不在
-
 
 var data=[
 	{
-		isMark:false,
-		Date:"2017/3/10",
-		content:"春眠不觉晓，处处闻啼鸟。夜来风雨声，花落知多少。"
+		id:0,
+		isMark:true,
+		Date:[2017,3,10,05,10],
+		content:"木木笔记是一款方便简洁高效的笔记应用，知识有限，不足之处敬请不吝指出，谢谢使用。本人邮箱：1171098609@qq.com"
 	},
 	{
+		id:1,
 		isMark:false,
-		Date:"2017/3/5",
+		Date:[2017,3,4,12,06],
 		content:"春眠不觉晓，处处闻啼鸟。"
 	},
 	{
+		id:2,
 		isMark:false,
-		Date:"2017/3/8",
-		content:"春眠不觉晓，处处闻啼鸟。夜来风雨声，花落知多少。"
+		Date:[2017,3,3,13,26],
+		content:"春秋战国时，有位俞伯牙善弹琴，一日乘舟游至汉阳江口，命童仆取琴焚香，调弦转轸，一曲未终弦断之。疑有人听琴，命左右搜之，遇一樵夫钟子期，二人登舟促膝相谈，意合知音，结为兄弟。约定来年江边相见。当俞伯牙按期来到江边时，钟子期已病故。伯牙闻知，泪如涌泉，去子期坟前祭拜，并割断琴弦，双手举琴向祭石台上一摔，摔得玉轸摧残，金徽零乱。老者惊问：“先生为何摔此琴？”伯牙道：“摔破瑶琴凤尾寒，子期不在对谁弹？春风满面皆朋友，欲觅知音难上难。"
 	}
 ]
-//tools.renderList(data);
-//-------------滑动删除标记-----------------
+
+//----------获取本地存储-----------
+var note=localStorage.getItem("note");
+if(note){
+	data=JSON.parse(note);
+}
+
+//-------------滑动菜单---------------
 $(".content").on("touchstart",".context",function(e){
 	if(!canMove) return;
-	ev=e.changedTouches[0];
+	var ev=e.changedTouches[0];
 	var _this=this,
 		oriX=ev.clientX,
 		moveX=null;
@@ -33,23 +43,28 @@ $(".content").on("touchstart",".context",function(e){
 		"transform":"translateX(0)"
 	})
 	$(document).on("touchmove",function(e){
+		
 		e=e.changedTouches[0];
 		moveX=e.clientX-oriX;
+		if(Math.abs(moveX)>15){
+			enter=false;
+		}
 		if(moveX>=0){
 			_this.style.transition="0.3s";
 			_this.style.transform="translateX(0)";
 		}else{
 			_this.style.transition="none";
-			moveX=-moveX>26.8*rem?-26.8*rem:moveX;
+			moveX=-moveX>5.36*rem?-5.36*rem:moveX;
 			_this.style.transform="translateX("+moveX+"px)"
 		}
 	})
 	$(document).one("touchend",function(){
 		$(document).off("touchmove");
-		if(-moveX>=13.4*rem){
+		if(-moveX>=2.68*rem){
 			_this.style.transition="0.2s";
-			_this.style.transform="translateX("+-26.8*rem+"px)";
+			_this.style.transform="translateX("+-5.36*rem+"px)";
 		}else{
+			enter=true; //如果已经滑动成功，则不能点击
 			_this.style.transition="0.3s";
 			_this.style.transform="translateX(0)";
 		}
@@ -60,68 +75,61 @@ $(".content").on("touchstart",".context",function(e){
 $(".search").on("touchend",function(e){
 	if(canSearch){
 		canMove=false; //搜索时不能滑动删除
+		enter=false;
 		$("header").hide();
-		$(this).css("width",62*rem+"px");
+		$(this).css("width",12.4*rem+"px");
 		$(".cancel").show();
 		$("footer").hide();
 		this.focus();
+		$(".context").css({
+			"transition":"0.3s",
+			"transform":"translateX(0)"
+		})
 	}
 })
 //取消搜索
 $(".cancel").on("touchend",function(){
 	canMove=true;
+	enter=true;
 	$("header").show();
-	$(".search").css("width",71.8*rem+"px");
+	$(".search").css("width",14.36*rem+"px");
 	$(".search").blur();
 	$(this).hide();
 	$("footer").show();
 })
 
 
-//--------点击编辑---------
+//--------编辑按钮---------
 $(".edit").on("touchend",function(){
 	if(!isEdit){
 		canSearch=false;
 		canMove=false;
+		enter=false;
 		$(this).text("取消")
 		$(".context").css({
 			"transition":"0.3s",
-			"transform":"translateX("+7.6*rem+"px)"
+			"transform":"translateX("+1.52*rem+"px)"
 		})
 		$(".serBox").css("opacity","0.4");
 		$(".delBtn").hide();
 		$(".mark").hide();
+		$(".markAll").show();
+		$(".deleteAll").show();
+		$(".count").hide();
+		$(".create").hide();
 	}else{
-		canSearch=true;
-		canMove=true;
-		$(this).text("编辑");
-		$("h1").text("木木笔记");
-		$(".markAll").text("标记全部");
-		$(".deleteAll").text("删除全部");
-		$(".context").css({
-			"transition":"0.3s",
-			"transform":"translateX(0)"
-		})
-		$(".serBox").css("opacity","1");
-		$(".context").removeClass("active");
-		setTimeout(function(){
-			$(".delBtn").show();
-			$(".mark").show();
-		},300)		
+		tools.cancelEdit();
 	}
 	isEdit=!isEdit;
-	$(".markAll").toggle();
-	$(".deleteAll").toggle();
-	$(".count").toggle();
-	$(".create").toggle();
 })
 
-//----------------选中操作-----------------
+//-------------选中--------------
+var len; //选中的个数
 $(".content").on("touchend",".context",function(){
 	if(isEdit){
 		$(".context").css("transition","none");
 		$(this).toggleClass("active");
-		var len=$(".active").length;
+		len=$(".active").length;
 		if(len!==0){
 			$("h1").text("已选定"+len+"项");
 			$(".markAll").text("标记选中");
@@ -134,4 +142,137 @@ $(".content").on("touchend",".context",function(){
 	}
 })
 
+
+//------------标记和取消--------------
+$(".content").on("touchend",".mark",function(){
+	var num=$(".mark").index(this);
+	var id=$(this).parents("li")[0].dataset.id; //获取当前的id
+	data.forEach(function(item,index){
+		if(item.id==id){
+			data[index].isMark=!data[index].isMark;
+			return
+		}
+	})
+	localStorage.setItem("note",JSON.stringify(data));
+	$(".context").eq(num).css({
+		"transition":"0.3s",
+		"transform":"translateX(0)"
+	})
+	setTimeout(function(){
+		tools.renderList(data); //渲染	
+	},200)
+})
+
+//标记
+$(".markAll").on("touchend",function(){
+	if(!len){  //全部标记
+		data.forEach(function(item){
+			item.isMark=true;
+		});	
+	}else{
+		tools.findSelect(function(index){
+			data[index].isMark=true;
+		})
+	}
+	localStorage.setItem("note",JSON.stringify(data));
+	tools.cancelEdit();
+	isEdit=false; //退出编辑状态
+	setTimeout(function(){
+		tools.renderList(data); //渲染	
+	},200)
+})
+
+//---------删除----------
+$(".deleteAll").on("touchend",function(){
+	if(!len){  //全部删除
+		tools.tips("删除全部");
+	}else{
+		tools.tips("删除选中");
+	}
+})
+var index; //删除序号
+//右滑删除
+$(".content").on("touchend",".delBtn",function(){
+	index=$(".delBtn").index(this); //获取当前的序号
+	tools.tips("删除");
+})
+//确定删除
+$(".sure").on("touchend",function(){
+	if(!isEdit){ //非编辑滑动删除
+		data.splice(index,1);
+	}
+	if(isEdit&&!len){ //编辑全部删除
+		data=[];
+		tools.cancelEdit();
+		isEdit=false;
+	}
+	if(isEdit&&len){ //编辑选中删除
+		tools.findSelect(function(index){
+			data.splice(index,1);
+		})
+		tools.cancelEdit();
+		isEdit=false;
+	}
+	localStorage.setItem("note",JSON.stringify(data));
+	tools.renderList(data); //渲染
+	$(".tips").css("transform","translateY(0)");
+	$("#mask").hide();
+})
+//-----取消删除----
+$(".giveUp").on("touchend",function(){
+	tools.cancelDelete();
+})
+
+//-----文本编辑-------
+$(".words").on("touchend",function(){
+	$(this).focus();
+})
+//返回
+$(".back").on("touchend",function(){
+	document.addEventListener("touchstart",prevd);
+	$(".words").blur();
+	setTimeout(function(){
+		$("#contShow").css("transform","translateX(0)");
+	},20)
+	if(create){
+		create=false;
+		if($(".words").text().trim()==="") return;
+		data.unshift({
+			id:parseInt(Math.random()*10000),
+			isMark:false,
+			Date:tools.setTime(),
+			content:$(".words").text()
+		})
+		
+	}else{
+		tools.changeData(infoId,{
+			Date:tools.setTime(),
+			content:$(".words").text()
+		})
+	}
+	tools.renderList(data);
+	localStorage.setItem("note",JSON.stringify(data));
+})
+
+//-----------新建文本-------------
+$(".create").on("touchend",function(){
+	create=true;
+	document.removeEventListener("touchstart",prevd);
+	$(".words").text("");
+	$(".time").text(tools.timeStr(tools.setTime()));
+	$(".words").focus();
+	tools.msgTextLastPos($(".words")[0]);
+	$("#contShow").css("transform","translateX("+-15*rem+"px)");
+})
+
+//点击进入
+var infoId; //记录访问的数据id
+$(".content").on("touchend",".context",function(){
+	if(!enter) return;
+	infoId=$(this).parent()[0].dataset.id;
+	document.removeEventListener("touchstart",prevd);
+	$(".time").text(tools.timeStr(tools.getInfo(infoId,"Date")));
+	$(".words").text(tools.getInfo(infoId,"content"));
+	$("#contShow").css("transform","translateX("+-15*rem+"px)");
+})
 tools.renderList(data);
--- notes/js/tools.js
+++ notes/js/tools.js
@@ -17,30 +17,134 @@
 			var str="";
 			for (var i = 0; i < data.length; i++){
 				var title=data[i].content.slice(0,20),
+					markStatus=data[i].isMark?"取消":"标记",
+					red=data[i].isMark?"red":null,
+					time=data[i].Date[0]+"/"+data[i].Date[1]+"/"+data[i].Date[2],
 					detial=data[i].content.slice(15,50);
 				if(detial===""){
 					detial="无附加文本";
 				}
 				str+=`
-					<li>
+					<li data-id=${data[i].id} class=${red}>
 						<div class="context">
 							<div class="checkbox"></div>
 							<div class="text">
 								<div class="title">${title}</div>
 								<div class="Info">
-									<span class="date">${data[i].Date}</span>
+									<span class="date">${time}</span>
 									<span class="detial">${detial}</span>
 								</div>
 							</div>
 						</div>
 						<div class="delete">
-							<div class="mark">标记</div>
+							<div class="mark">${markStatus}</div>
 							<div class="delBtn">删除</div>
 						</div>
 					</li>
 				`
 			}
 			$(".content").html(str);
+		},
+		//根据传入id找到数据，并修改数据
+		changeData:function(id,obj){
+			data.forEach(function(item,index){
+				if(item.id==id){
+					for(var attr in obj){
+						item[attr]=obj[attr];
+					}
+				}
+			})
+		},
+		//--------取消编辑状态-------
+		cancelEdit:function(){
+			canSearch=true;
+			canMove=true;
+			enter=true;
+			$(".edit").text("编辑");
+			$("h1").text("木木笔记");
+			$(".markAll").text("标记全部");
+			$(".deleteAll").text("删除全部");
+			$(".context").css({
+				"transition":"0.3s",
+				"transform":"translateX(0)"
+			})
+			$(".serBox").css("opacity","1");
+			$(".context").removeClass("active");
+			$(".markAll").hide();
+			$(".deleteAll").hide();
+			$(".count").show();
+			$(".create").show();
+			setTimeout(function(){
+				$(".delBtn").show();
+				$(".mark").show();
+			},300)		
+		},
+		//-------操作时弹出提示-------
+		tips:function(str){
+			$(".tips").show();
+			$("#mask").show();
+			$(".sure").text(str);
+			setTimeout(function(){
+				$(".tips").css("transform","translateY("+-5.2*rem+"px)");
+			},50)
+			
+		},
+		//取消删除
+		cancelDelete:function(){
+			if(!isEdit){
+				$(".context").css({
+					"transition":"0.3s",
+					"transform":"translateX(0)"
+				})
+			}
+			$(".tips").css("transform","translateY(0)");
+			$("#mask").hide();
+		},
+		//获取被选中数据的id
+		findSelect:function(fn){
+			$(".active").each(function(){
+				var id=$(this).parent()[0].dataset.id;
+				data.forEach(function(item,index){
+					if(item.id==id){
+						fn(index);
+						return
+					}
+				})
+			})
+		},
+		// 光标移动到最后
+		msgTextLastPos:function(obj) {
+	        obj.focus(); //解决ff不获取焦点无法定位问题
+	        var range = window.getSelection();//创建range
+	        range.selectAllChildren(obj);//range 选择obj下所有子内容
+	        range.collapseToEnd();//光标移至最后
+		},
+		setTime:function(){
+			var now=new Date(),
+			h=tools.addZero(now.getHours()),
+			m=tools.addZero(now.getMinutes()),
+			y=now.getFullYear(),
+			u=now.getMonth()+1,
+			d=now.getDate();
+			var arr=[y,u,d,h,m];
+			return arr;
+		},
+		timeStr:function(arr){
+			var str=arr[0]+'年'+arr[1]+'月'+arr[2]+'日'+' '+tools.addZero(arr[3])+':'+tools.addZero(arr[4]);			
+			return str;
+		},
+		addZero:function(n){
+			return n<10?"0"+n:""+n;
+		},
+		getInfo:function(id,attr){
+			var ss; //用来保存数据并return
+			data.forEach(function(item,index){
+				if(item.id==id){
+					ss=data[index][attr];
+					return ;
+				}
+			})
+			return ss;
 		}
 	}	
 	window.tools=tools;
